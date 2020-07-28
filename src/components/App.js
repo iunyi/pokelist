4fbe026f029e483d206b845d7a2ef4ebd5d59aad
Junyi Ji <junyiji@gmail.com>
2020-07-28 08:42:49 +0200

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import '../stylesheets/App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <p>
+          Hello React &nbsp;
+          <i class="fas fa-ghost"></i>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
